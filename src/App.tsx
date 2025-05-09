bd5b46d4303176325da480235dadad00c842f2b8
Praveen Nair <praveen.nair@clouwood.com>
2025-05-09 23:41:37 +0530

    Added react skeleton

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-gray-100 w-full h-[100px] flex items-center justify-center">
+     <p className='text-2xl font-bold'>Hello World!</p>
+    </div>
+  );
+}
+
+export default App;
